fd3bc2f8176a0c28d4640a6b8cfe785b83113bbf
Davit <ghrzoyandavit@gmail.com>
2022-11-22 19:18:30 +0400

    Add project files.

---
+++ testingApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace testingApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello Davit " + "Hello Martin" + " Whats up bro");
+            //Console.Write("Hello Martin");
+            //Console.Write(" Whats up ");
+            //Console.ReadLine();
+            //Console.Read();
+            //Console.ReadKey();
+
+                
+        }
+    }
+}
